babf02276a4d4839dfb474e535ee017409870414
vaceslavlomovcev <balderfreir@yandex.ru>
2023-02-12 19:50:00 +0400

    qr code decoder

---
+++ qrcodedecoder.py
@@ -0,0 +1,13 @@
+import cv2
+
+# read the QRCODE image
+image = cv2.imread("vk_qr.png")
+# initialize the cv2 QRCode detector
+detector = cv2.QRCodeDetector()
+# detect and decode
+data, vertices_array, binary_qrcode = detector.detectAndDecode(image)
+# if there is a QR code print the data
+if vertices_array is not None:
+    print("QRCode data:", data)
+else:
+    print("There was some error")
