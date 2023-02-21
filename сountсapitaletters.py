937a41ae27cb763bc90315d4fe5ea38c3597a1ba
vaceslavlomovcev <balderfreir@yandex.ru>
2023-02-21 22:56:39 +0400

    add comments

--- сountсapitaletters.py
+++ сountсapitaletters.py
@@ -1,7 +1,12 @@
 with open('the_fellowship_of_the_ring.txt') as file:
+    # создаем счетчик
     count = 0
+    # заносим весь текст в переменную
     text = file.read()
+    # проходимся по символьно
     for i in text:
+        # является ли символ заглавным?
         if i.isupper():
+            # если да, то увеличиваем на 1
             count += 1
 print(count)
