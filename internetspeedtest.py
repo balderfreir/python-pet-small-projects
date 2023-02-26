3ca684e7ec4e84859ca0c64bf75b80087f25ccdf
vaceslavlomovcev <balderfreir@yandex.ru>
2023-02-26 17:16:45 +0400

    internet speed test, now it works

--- internetspeedtest.py
+++ internetspeedtest.py
@@ -1,18 +1,13 @@
 import speedtest
 
-servers = []
-# If you want to test against a specific server
-# servers = [1234]
-
-threads = None
-# If you want to use a single threaded test
-# threads = 1
-
-s = speedtest.Speedtest()
-s.get_servers(servers)
-s.get_best_server()
-s.download(threads=threads)
-s.upload(threads=threads)
-s.results.share()
-
-results_dict = s.results.dict()
+st = speedtest.Speedtest()
+option = int(input('Чего желаем знать?   \n'
+                   '1 - Скорость скачивания \n'
+                   '2 - Скорость загрузки   \n'
+                   'Ответ: '))
+if option == 1:
+    print(st.download() / 1024 / 1024)
+elif option == 2:
+    print(st.upload() / 1024 / 1024)
+else:
+    print("Введите 1 или 2")
