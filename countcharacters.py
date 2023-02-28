19e0387996b57bbc332eebdacb2dc6fdf46ac4d3
vaceslavlomovcev <balderfreir@yandex.ru>
2023-02-28 22:35:24 +0400

    add count characters

---
+++ countcharacters.py
@@ -0,0 +1,15 @@
+def count_characters(string):
+    """Не учитывает регистр или пробелы,
+    т.е. Е != е и ' ' - так же отдельный символ"""
+    counter = {}  # создаем словать
+    for ch in string:  # идем по каждому эл строки
+        # если он есть в словаре, увеличиваем значение
+        if ch in counter:
+            counter[ch] += 1
+        # если его нет, то записываем со значением 1
+        else:
+            counter[ch] = 1
+    return counter
+
+string = 'Some string for counting letters'
+print(count_characters(string))
