13973b804910c28411546cd119f03869794f6d11
vaceslavlomovcev <balderfreir@yandex.ru>
2023-03-08 20:20:58 +0400

    add validate anagrams func

---
+++ validateanagrams.py
@@ -0,0 +1,20 @@
+def validate_anagrams(a=None, b=None):
+    ans = ''
+    # проверяем, введены ли значения
+    if not a or not b:
+        ans = 'Ты шо то забыл...'
+    # проверяем, не одно ли и тоже слова
+    elif a.lower() == b.lower():
+        ans = 'Почему слова одинаковые?'
+    # проверка на анаграмность!
+    elif sorted(a.lower()) == sorted(b.lower()):
+        ans = 'Да это же анаграмы!!!'
+    else:
+        ans = 'Не анаграмы...'
+    return ans
+
+print(validate_anagrams("cinema"))
+print(validate_anagrams("cool", "loco"))
+print(validate_anagrams("men", "women"))
+print(validate_anagrams("Finger", "finger"))
+print(validate_anagrams("cinema", "iceman"))
