911c0c2d2b4d192629d2c522e8f7e7937511a79b
vaceslavlomovcev <balderfreir@yandex.ru>
2023-03-13 17:47:05 +0400

    commit

--- correctgrammar.py
+++ correctgrammar.py
@@ -6,3 +6,5 @@ text = input("Enter a sentence: ")
 corrected_text = GingerIt().parse(text)
 # Выводим результат
 print(corrected_text['result'])
+[print(msk) for msk in corrected_text['corrections']]
+# how ar u? im fine fank yoy and yiu?
