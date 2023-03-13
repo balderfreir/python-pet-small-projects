e146be8cf06bd4b8482f5c262025e478648700bc
vaceslavlomovcev <balderfreir@yandex.ru>
2023-03-13 17:31:15 +0400

    add calcdistance.py

--- calcdistance.py
+++ calcdistance.py
@@ -12,3 +12,9 @@ def dist_calculate(lat1, lon1, lat2, lon2):
     a = np.sin(d_lat/2)**2 + np.cos(deg_to_rad(lat1)) * np.cos(deg_to_rad(lat2)) * np.sin(d_lon/2)**2
     c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
     return r * c
+
+lat1 = 55.7539
+lon1 = 37.6208
+lat2 = 59.9398
+lon2 = 30.3146
+print(dist_calculate(lat1, lon1, lat2, lon2))
