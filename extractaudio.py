2de40319475ded35cf482283c3692ca6c0e854db
vaceslavlomovcev <balderfreir@yandex.ru>
2023-03-13 17:37:12 +0400

    commit

--- extractaudio.py
+++ extractaudio.py
@@ -5,21 +5,14 @@ import os
 def main():
     video_url = input('Enter YouTube video URL: ')
 
-    if os.name == 'nt':
-        path = os.getcwd() + '\\'
-    else:
-        path = os.getcwd() + '/'
+    path = os.getcwd() + '/'
 
     name = pytube.extract.video_id(video_url)
     YouTube(video_url).streams.filter(only_audio=True).first().download(filename=name)
     location = path + name
     renametomp3 = path + name + '.mp3'
 
-    if os.name == 'nt':
-        os.system('ren {0} {1}'.format(location, renametomp3))
-    else:
-        os.system('mv {0} {1}'.format(location, renametomp3))
-
+    os.system('mv {0} {1}'.format(location, renametomp3))
 
 if __name__ == '__main__':
     main()
