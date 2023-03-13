96f9bae088f3add2e15a5d95e1ad0b63a2b2f95a
vaceslavlomovcev <balderfreir@yandex.ru>
2023-03-13 17:34:14 +0400

    commit

--- urlshorter.py
+++ urlshorter.py
@@ -3,14 +3,12 @@ import contextlib
 from urllib.parse import urlencode
 from urllib.request import urlopen
 
-
 def make_tiny(url):
     request_url = ('http://tinyurl.com/api-create.php?' +
                    urlencode({'url': url}))
     with contextlib.closing(urlopen(request_url)) as response:
         return response.read().decode('utf-8')
-
-
+# urlencode избавляется от спецсимволов, urlopen открывает ссылку
 def main():
     url = input('Enter url: ')
     print('Short var:', make_tiny(url))
